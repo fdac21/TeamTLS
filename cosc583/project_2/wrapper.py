472fdaf5d00b1f8d93cee2f29cbe2eb34f3ce584
bgreenb11 <blg1111@outlook.com>
2021-09-09 23:45:03 -0400

    Progress on 583-Project 2

--- cosc583/project_2/wrapper.py
+++ cosc583/project_2/wrapper.py
@@ -1,7 +1,7 @@
 from hashlib import sha256
-import os
 import random
 from string import ascii_letters, digits
+import numpy as np
 
 ALPHANUM: str = ascii_letters + digits
 
@@ -25,44 +25,62 @@ def sha256w(data: str = "", bits: int = 256) -> str:
     return enc
 
 
-def collision() -> None:
+def collision() -> dict[int, list[int]]:
     bits = [8, 10, 12, 14, 16, 18, 20, 22]
+    hashes = []
+    bits = {size: [] for size in bits}
     tries = 0
 
-    for bit in bits:
-        
-        while True:
-            tries += 1
-            alt = ranString(25)
+    for size in bits.keys():
+        for i in range(50):
             base = ranString(25)
-            res = sha256w(base, bit)
-            test = sha256w(alt, bit)
-            if res == test:
-                break
-        print(
-            f"Bit length {bit} took {tries} tries to collide with strings {base} and {alt}")
-        tries = 0
+            res = sha256w(base, size)
+            hashes.append(res)
+
+            while True:
+                tries += 1
+
+                alt = ranString(25)
+                test = sha256w(alt, size)
+                if test in hashes:
+                    break
+                else:
+                    hashes.append(test)
+
+            bits[size].append(tries)
+            tries = 0
+    return bits
 
 
-def preImage() -> None:
+def preImage() -> dict[int, list[int]]:
     bits = [8, 10, 12, 14, 16, 18, 20, 22]
+    bits = {size: [] for size in bits}
     tries = 0
 
-    for bit in bits:
-        base = ranString(15)
-        res = sha256w(base, bit)
-        while True:
-            tries += 1
-            alt = ranString(15)
-            test = sha256w(alt, bit)
-            if res == test:
-                break
-        print(
-            f"Pre-image attack with bit length {bit} took {tries} tries")
-        tries = 0
+    for size in bits.keys():
+        for i in range(50):
+            base = ranString(25)
+            res = sha256w(base, size)
+            while True:
+                tries += 1
+                alt = ranString(25)
+                test = sha256w(alt, size)
+                if res == test:
+                    break
+            # print(
+            #     f"Pre-image attack with bit length {size} took {tries} tries")
+            bits[size].append(tries)
+            tries = 0
+
+    return bits
 
 
-collision()
-preImage()
+col_trials = collision()
+pre_trials = preImage()
 
+for trial in col_trials.keys():
+    print(
+        f"Collision avg for bit size {trial}: {np.mean(col_trials[trial])}")
+    print(
+        f"Pre-image avg for bit size {trial}: {np.mean(pre_trials[trial])}")
 # sha256w("TEST", 32)
