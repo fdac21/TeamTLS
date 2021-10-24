2826ed2cd57e22939298b676baf5c0ac35d4b5e3
bgreenb11 <blg1111@outlook.com>
2021-10-23 21:23:29 -0400

    Desktop upgrade commit

--- cosc530/pa_2/dynamsched.cpp
+++ cosc530/pa_2/dynamsched.cpp
@@ -2,29 +2,27 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 int main() {
-    initScheduler(NULL, NULL);
+    DynamSched sched = DynamSched("source/config.txt");
 }
 
-void getValue(fstream& f, string& line) {
+void getValue(fstream& f, string& line, stringstream& ss) {
     getline(f, line, ':');
     getline(f, line);
+    ss << line;
 }
 
-void initScheduler(Buffer* buf, Latency* lat) {
-    string source = "source/config.txt";
-    string buffer = "buffer";
-    string latency = "latency";
+DynamSched::DynamSched(string configFile) {
     string line;
-
     fstream conf;
 
-    conf.open(source);
+    conf.open(configFile);
 
     if (!conf.is_open()) {
         perror("Error opening config file.\n");
@@ -34,9 +32,40 @@ void initScheduler(Buffer* buf, Latency* lat) {
     while (getline(conf, line) && line != "buffers")
         ;
 
+    // Skip empty line
     getline(conf, line);
 
-    getValue(conf, line);
+    // Allocate Effective Address Buffer
+    this->allocateBuffer(conf, this->effAddr);
 
-    cout << line << endl;
+    // Allocate Floating-Point Adds Buffer
+    this->allocateBuffer(conf, this->fpAdds);
+
+    // Allocate Floating-Point Muls Buffer
+    this->allocateBuffer(conf, this->fpMuls);
+
+    // Allocate Int Buffer
+    this->allocateBuffer(conf, this->Ints);
+
+    // Allocate Reorder Buffer
+    this->allocateBuffer(conf, this->reorder);
+}
+
+void DynamSched::allocateBuffer(fstream& conf, vector<ResStation>& buffer) {
+    string line;
+    stringstream ss;
+    int size = 0;
+
+    getValue(conf, line, ss);
+    ss >> size;
+
+    buffer.resize(size);
+
+    for (int i = 0; i < buffer.size(); i++) {
+        buffer.at(i) = ResStation();
+    }
+}
+
+ResStation::ResStation() {
+    this->instruction = 0;
 }
--- cosc530/pa_2/dynamsched.hpp
+++ cosc530/pa_2/dynamsched.hpp
@@ -1,14 +1,46 @@
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 
-class Buffer {
+enum FLAGS {
+    lw = 0x001,
+    flw = 0x002,
+    sw = 0x004,
+    fsw = 0x008,
+    DT = lw | flw | sw | fsw,
+    add = 0x010,
+    sub = 0x020,
+    ARITH = add | sub,
+    beq = 0x040,
+    bne = 0x080,
+    CTRL = beq | bne,
+    fadd_s = 0x100,
+    fsub_s = 0x200,
+    fmul_s = 0x400,
+    fdiv_s = 0x800,
+    FP = fadd_s | fsub_s | fmul_s | fdiv_s,
+};
+
+class ResStation {
+    int instruction;
+
    public:
-    int effAddr;
-    int fpAdds;
-    int fpMuls;
-    int Ints;
-    int reorder;
+    ResStation();
+};
 
-    Buffer(int effAddr, int fpAdds, int fpMuls, int Ints, int reorder);
+class DynamSched {
+   private:
+    // Reservation Stations
+    std::vector<ResStation> effAddr;
+    std::vector<ResStation> fpAdds;
+    std::vector<ResStation> fpMuls;
+    std::vector<ResStation> Ints;
+    std::vector<ResStation> reorder;
+
+   public:
+    DynamSched(std::string configFile);
+    void allocateBuffer(std::fstream& conf, std::vector<ResStation>& buffer);
 };
 
 class Latency {
@@ -20,5 +52,3 @@ class Latency {
 
     Latency(int fp_add, int fp_sub, int fp_mul, int fp_div);
 };
-
-void initScheduler(Buffer *buf, Latency *lat);
