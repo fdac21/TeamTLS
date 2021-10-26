e478358b1310b6676e256c379c1511c49b6725cf
bgreenb11 <blg1111@outlook.com>
2021-10-26 15:54:00 -0400

    To desktop commit

--- cosc530/pa_2/dynamsched.cpp
+++ cosc530/pa_2/dynamsched.cpp
@@ -1,42 +1,134 @@
 #include "dynamsched.hpp"
 
+#include <deque>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 int main() {
-    initScheduler(NULL, NULL);
+  DynamSched sched = DynamSched("source/config.txt");
+  sched.process();
+
+  return 0;
 }
 
-void getValue(fstream& f, string& line) {
-    getline(f, line, ':');
-    getline(f, line);
+int getValue(fstream& f) {
+  string line;
+  stringstream ss;
+  int val;
+  getline(f, line, ':');
+  getline(f, line);
+  ss << line;
+  ss >> val;
+
+  return val;
 }
 
-void initScheduler(Buffer* buf, Latency* lat) {
-    string source = "source/config.txt";
-    string buffer = "buffer";
-    string latency = "latency";
-    string line;
+DynamSched::DynamSched(string configFile) {
+  string line;
+  fstream conf;
 
-    fstream conf;
+  conf.open(configFile);
 
-    conf.open(source);
+  if (!conf.is_open()) {
+    perror("Error opening config file.\n");
+    exit(-1);
+  }
 
-    if (!conf.is_open()) {
-        perror("Error opening config file.\n");
-        exit(-1);
-    }
+  while (getline(conf, line) && line != "buffers")
+    ;
 
-    while (getline(conf, line) && line != "buffers")
-        ;
+  // Skip empty line
+  getline(conf, line);
 
-    getline(conf, line);
+  // Allocate Effective Address Buffer
+  this->allocateBuffer(conf, this->effAddr);
 
-    getValue(conf, line);
+  // Allocate Floating-Point Adds Buffer
+  this->allocateBuffer(conf, this->fpAdds);
 
-    cout << line << endl;
+  // Allocate Floating-Point Muls Buffer
+  this->allocateBuffer(conf, this->fpMuls);
+
+  // Allocate Int Buffer
+  this->allocateBuffer(conf, this->arithmetics);
+
+  // Allocate Reorder Buffer
+  this->allocateReorderBuffer(conf);
+
+  // Set Latencies
+  this->setLatencies(conf);
+
+  // Allocate Floating-Point Registers
+  this->allocateRegisters(this->fps);
+
+  // Allocate Integer Registers
+  this->allocateRegisters(this->ints);
+
+  cout << this->latencies.fp_add << endl;
 }
+
+void DynamSched::allocateBuffer(fstream& conf, vector<ResStation>& buffer) {
+  int size = 0;
+
+  size = getValue(conf);
+
+  buffer.resize(size);
+
+  for (int i = 0; i < buffer.size(); i++) {
+    buffer.at(i) = ResStation();
+  }
+}
+
+void DynamSched::allocateReorderBuffer(fstream& conf) {
+  int size = 0;
+
+  size = getValue(conf);
+
+  this->reorder.resize(size);
+
+  for (int i = 0; i < this->reorder.size(); i++) this->reorder.at(i) = NULL;
+}
+
+void DynamSched::setLatencies(fstream& conf) {
+  int cycles;
+
+  cycles = getValue(conf);
+  this->latencies.fp_add = cycles;
+
+  cycles = getValue(conf);
+  this->latencies.fp_sub = cycles;
+
+  cycles = getValue(conf);
+  this->latencies.fp_mul = cycles;
+
+  cycles = getValue(conf);
+  this->latencies.fp_div = cycles;
+}
+
+void DynamSched::allocateRegisters(vector<Register>& registers) {
+  registers.resize(16);
+  for (int i = 0; i < 16; i++) {
+    registers.at(i) = Register();
+  }
+}
+
+void DynamSched::process() {}
+
+/* Reorder Buffer */
+
+ReorderBuffer::ReorderBuffer(int size) { this->size = size; }
+
+void ReorderBuffer::commitEntry() {
+  // Remove entry at head of buffer and commit the results
+  // Possibly return Entry to caller and take care of scheduler interaction
+  // outside of buffer
+}
+
+/* End Reorder Buffer */
+
+ResStation::ResStation() {}
--- cosc530/pa_2/dynamsched.hpp
+++ cosc530/pa_2/dynamsched.hpp
@@ -1,24 +1,92 @@
+#include <deque>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 
-class Buffer {
-   public:
-    int effAddr;
-    int fpAdds;
-    int fpMuls;
-    int Ints;
-    int reorder;
-
-    Buffer(int effAddr, int fpAdds, int fpMuls, int Ints, int reorder);
+enum INST {
+  lw = 0x001,
+  flw = 0x002,
+  sw = 0x004,
+  fsw = 0x008,
+  DT = lw | flw | sw | fsw,
+  add = 0x010,
+  sub = 0x020,
+  ARITH = add | sub,
+  beq = 0x040,
+  bne = 0x080,
+  CTRL = beq | bne,
+  fadd_s = 0x100,
+  fsub_s = 0x200,
+  fmul_s = 0x400,
+  fdiv_s = 0x800,
+  INT = beq | bne | add | sub | lw | sw,
+  FP = fadd_s | fsub_s | fmul_s | fdiv_s | flw | fsw,
 };
 
-class Latency {
-   public:
-    int fp_add;
-    int fp_sub;
-    int fp_mul;
-    int fp_div;
-
-    Latency(int fp_add, int fp_sub, int fp_mul, int fp_div);
+struct Latency {
+  int fp_add;
+  int fp_sub;
+  int fp_mul;
+  int fp_div;
 };
 
-void initScheduler(Buffer *buf, Latency *lat);
+struct Instruction {
+  uint32_t inst;
+};
+
+struct Entry {
+  INST type;
+  int reg;
+  bool ready;
+};
+
+class ResStation {
+  Instruction *instruction;
+  int val1, val2;
+  int immediate;
+  bool busy;
+  ResStation *RS1, *RS2;
+
+ public:
+  ResStation();
+};
+
+// TODO: Finish out register struct
+struct Register {
+  ResStation *RS = NULL;
+};
+
+class ReorderBuffer {
+  int size;
+  std::deque<Entry> buffer;
+
+ public:
+  ReorderBuffer(int size);
+  void insertEntry(Entry e);
+  void commitEntry();
+}
+
+class DynamSched {
+ private:
+  Latency latencies;
+  // Reservation Stations
+  std::vector<ResStation> effAddr;
+  std::vector<ResStation> fpAdds;
+  std::vector<ResStation> fpMuls;
+  std::vector<ResStation> arithmetics;
+  std::deque<Entry> reorder;
+
+  std::vector<ResStation *> CommonDataBus;
+
+  std::vector<Register> fps;
+  std::vector<Register> ints;
+
+ public:
+  DynamSched(std::string configFile);
+  void allocateBuffer(std::fstream &conf, std::vector<ResStation> &buffer);
+  void allocateReorderBuffer(std::fstream &conf);
+  void setLatencies(std::fstream &conf);
+  void allocateRegisters(std::vector<Register> &registers);
+  void process();
+};
