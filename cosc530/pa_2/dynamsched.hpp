a5d542c42bce68ff95570fc7ff1699dc882f2023
bgreenb11 <blg1111@outlook.com>
2021-11-02 22:28:02 -0400

    Adding subtree

--- cosc530/pa_2/dynamsched.hpp
+++ cosc530/pa_2/dynamsched.hpp
@@ -5,115 +5,120 @@
 #include <vector>
 
 enum INST {
-  LW = 0x001,
-  FLW = 0x002,
-  SW = 0x004,
-  FSW = 0x008,
-  DT = LW | FLW | SW | FSW,
-  ADD = 0x010,
-  SUB = 0x020,
-  ARITH = ADD | SUB,
-  BEQ = 0x040,
-  BNE = 0x080,
-  CTRL = BEQ | BNE,
-  FADD_S = 0x100,
-  FSUB_S = 0x200,
-  FMUL_S = 0x400,
-  FDIV_S = 0x800,
-  INT = BEQ | BNE | ADD | SUB | LW | SW,
-  FPA = FADD_S | FSUB_S,
-  FPM = FMUL_S | FDIV_S,
-  FP = FADD_S | FSUB_S | FMUL_S | FDIV_S | FLW | FSW,
+    LW = 0x001,
+    FLW = 0x002,
+    SW = 0x004,
+    FSW = 0x008,
+    DT = LW | FLW | SW | FSW,
+    ADD = 0x010,
+    SUB = 0x020,
+    ARITH = ADD | SUB,
+    BEQ = 0x040,
+    BNE = 0x080,
+    CTRL = BEQ | BNE,
+    FADD_S = 0x100,
+    FSUB_S = 0x200,
+    FMUL_S = 0x400,
+    FDIV_S = 0x800,
+    INT = BEQ | BNE | ADD | SUB | LW | SW,
+    FPA = FADD_S | FSUB_S,
+    FPM = FMUL_S | FDIV_S,
+    FP = FADD_S | FSUB_S | FMUL_S | FDIV_S | FLW | FSW,
 };
 
 enum HAZARD {
-  STRUCT,
-  DATA,
-  NONE,
+    STRUCT,
+    DATA,
+    NONE,
 };
 
 struct Latency {
-  int fp_add;
-  int fp_sub;
-  int fp_mul;
-  int fp_div;
+    int fp_add;
+    int fp_sub;
+    int fp_mul;
+    int fp_div;
 };
 
 struct Instruction {
-  INST opcode;
-  std::vector<int> regs;
+    INST opcode;
+    int requiredCycles;
+    int cycles;
+    int startEx;
+    int endEx;
+    int written;
+    int commited;
+    std::vector<int> regs;
 };
 
 struct ResStation {
-  Instruction inst;
-  int val1 = -1, val2 = -1;
-  int immediate;
-  uint64_t address;
-  bool busy = false;
-  ResStation *rs1 = NULL, *rs2 = NULL;
+    Instruction inst;
+    int val1 = -1, val2 = -1;
+    int immediate = -1;
+    uint64_t address = 0;
+    bool busy = false;
+    ResStation *rs1 = NULL, *rs2 = NULL;
 };
 
 ResStation createRS(Instruction inst);
 
 struct Entry {
-  INST type;
-  // int reg;
-  ResStation *rs;
-  bool ready;
+    INST inst;
+    // int reg;
+    ResStation *rs = NULL;
+    bool ready = false;
 };
 
 Entry createEntry(Instruction inst, ResStation &rs);
 
-class ResStationBuffer {
- public:
-  std::deque<ResStation> buffer;
-  int size;
-  ResStationBuffer(int size);
-  // TODO: Check immediate value parsing and handling
-  ResStation createRS(Instruction inst);
-  void insert(Instruction inst);
+struct ResStationBuffer {
+    std::deque<ResStation> buffer;
+    int size;
+    // TODO: Check immediate value parsing and handling
+    HAZARD checkBuffer();
+    void freeRS();
+    void insert(ResStation rs);
 };
 
 // TODO: Finish out register struct
 struct Register {
-  ResStation *RS = NULL;
+    ResStation *RS = NULL;
 };
 
-class ReorderBuffer {
- public:
-  int size;
-  std::deque<Entry> buffer;
+struct ReorderBuffer {
+    int size;
+    std::deque<Entry> buffer;
 
-  ReorderBuffer(int size);
-  void insert(Instruction inst);
-  void commitEntry();
+    void insert(Instruction inst, ResStationBuffer &buffer);
+    void commitEntry();
 };
 
 class DynamSched {
- private:
-  Latency latencies;
-  // Reservation Stations
-  ResStationBuffer effAddr;
-  ResStationBuffer fpAdds;
-  ResStationBuffer fpMuls;
-  ResStationBuffer arithmetics;
-  ReorderBuffer reorder;
+   private:
+    Latency latencies;
+    // Reservation Stations
+    ResStationBuffer effAddr;
+    ResStationBuffer fpAdds;
+    ResStationBuffer fpMuls;
+    ResStationBuffer arithmetics;
+    ReorderBuffer reorder;
 
-  Entry *CommonDataBus;
+    Entry *CommonDataBus;
 
-  std::vector<Register> fps;
-  std::vector<Register> ints;
+    std::vector<Register> fps;
+    std::vector<Register> ints;
 
- public:
-  DynamSched(std::string configFile);
-  void allocateBuffer(std::fstream &conf, std::vector<ResStation> &buffer);
-  void allocateReorderBuffer(std::fstream &conf);
-  void setLatencies(std::fstream &conf);
-  void allocateRegisters(std::vector<Register> &registers);
-  void process();
-  bool issue(std::string op, std::string regs);
-  void execute();
-  void checkCDB(ResStationBuffer &buffer);
+   public:
+    DynamSched(std::string configFile);
+    void allocateBuffer(std::fstream &conf, ResStationBuffer &buffer);
+    void allocateReorderBuffer(std::fstream &conf);
+    void setLatencies(std::fstream &conf);
+    void allocateRegisters(std::vector<Register> &registers);
+    void process();
+    bool issue(std::string op, std::string regs);
+    void execute();
+    void checkCDB(ResStationBuffer &buffer);
+    void write();
+    void commit();
 };
 
 INST strToInst(std::string inst);
@@ -121,7 +126,7 @@ INST strToInst(std::string inst);
 void parseRegisters(INST inst, std::string regs);
 
 template <typename T>
-HAZARD checkRSBuffer(deque<T> &buffer, int size);
+HAZARD checkRSBuffer(std::deque<T> &buffer, int size);
 
 template <typename T>
-HAZARD checkReorderBuffer(deque<T> &buffer, int size);
+HAZARD checkReorderBuffer(std::deque<T> &buffer, int size);
--- cosc530/pa_2/dynamsched.py
+++ cosc530/pa_2/dynamsched.py
@@ -1,2 +1,69 @@
 #!/usr/bin/env python3.6
+import sys
 
+
+class ResStation():
+    pass
+
+
+class Entry():
+    pass
+
+
+class ReorderBuffer():
+    def __init__(self, size):
+        self.size = size
+        self.buffer = []
+
+
+class ResStationBuffer():
+    def __init__(self, size):
+        # TODO: Might remove size attribute
+        self.size = size
+        self.buffer = [ResStation() for _ in range(size)]
+
+
+class Register():
+    def __init__(self, index):
+        self.index = index
+        ResStation
+
+
+class DynamSched():
+
+    def __init__(self, configFile):
+        with open(configFile) as conf:
+            options = [opt.split(':')
+                       for opt in conf.readlines() if ':' in opt]
+
+            self.buffers = {
+                k.strip(): int(v.strip()) for k, v in options[:5]
+            }
+            self.latencies = {
+                k.strip(): int(v.strip()) for k, v in options[5:]
+            }
+            print(self.latencies)
+
+            self.buffers = {
+                k: {
+                    "size": v,
+                    "buffer": ResStationBuffer(v)
+                } for k, v in self.buffers.items()
+            }
+
+            # for k, v in self.buffers.items():
+            #     print(k, v["size"])
+
+    def process(self):
+        for line in sys.stdin:
+            print(line.strip())
+
+
+def main():
+    configFile = "source/config.txt"
+    sched = DynamSched(configFile)
+    sched.process()
+
+
+if __name__ == '__main__':
+    main()
